970665230c6123c7e5c5e705919130c8ff2f1431
BDF007 <bdf007@gmail.com>
2022-03-17 11:42:05 +0100

    ajout des prenoms des wilders

---
+++ index.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+const log = console.log;
+
+log(chalk.red('Vincent'));
+log(chalk.underline.bgGreen.blueBright('Jerome'));
+log(chalk.bold.bgYellowBright.gray('Severine'));
+log(chalk.italic.redBright('Erwan'));
